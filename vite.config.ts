93cade34f02017cb8f247c870dcfaf8f84d7f6c2
leanq <leanqha@gmail.com>
2025-12-19 09:17:52 +0300

    дп

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Замените <repo-name> на имя репозитория
 export default defineConfig({
-    base: '/techup-frontend/',
+    base: '/', // ← ВАЖНО
     plugins: [
         react({
             babel: {
