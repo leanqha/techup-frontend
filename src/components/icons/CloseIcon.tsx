b0efefe9e8b8c4b59092e951d337f8ead79e0cc4
leanq <leanqha@gmail.com>
2026-03-06 09:34:23 +0300

    Update CloseIcon size for improved visibility

--- src/components/icons/CloseIcon.tsx
+++ src/components/icons/CloseIcon.tsx
@@ -2,7 +2,7 @@ type CloseIconProps = {
     size?: number;
 };
 
-export function CloseIcon({ size = 18 }: CloseIconProps) {
+export function CloseIcon({ size = 20 }: CloseIconProps) {
     return (
         <svg width={size} height={size} viewBox="0 0 24 24" fill="none" aria-hidden="true">
             <path
